bcea22ac9b81fe82ed3134c28ae1ccc3ed69e59b
dbezzi <dariobezzi@gmail.com>
2015-05-26 15:33:35 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,24 +1,91 @@
-var http = require('http');
+var _ = require('underscore');
+var express = require('express');
+var https = require('https');
+var app = express();
 
-var userCount = 0;
+app.set('ORGANISATION', 'nukomeet');
+app.set('GITHUB_AUTH_TOKEN', process.env.GH_AUTH_TOKEN);
+app.set('HIPCHAT_KEY', process.env.HIPCHAT_KEY);
 
-var server = http.createServer(function (req, res) {
+var _options = {
+  headers: {
+    'User-Agent': app.get('ORGANISATION'),
+    'Authorization': 'token '+ app.get('GITHUB_AUTH_TOKEN')
+  },
+  hostname: 'api.github.com'
+};
 
-    userCount++;
+app.get('/pull-requests', function (request, response) {
+  fetchRepos(fetchPullRequests);
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  app.once('pull-requests:fetched', function (pullRequests) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    var html = "";
 
-    res.write('Hello!\n');
+    _.each(pullRequests, function (pullRequests, index) {
+      html += 'There is <strong>'+ pullRequests.data.length +'</strong> pending pull request(s) for <strong>'+ pullRequests[index].repo +'</strong>:';
+      html += '<ul>';
+      _.each(pullRequests.data, function (pullRequest) {
+        html += '<li><em>'+ pullRequest.title +'</em> (<a href="'+ pullRequest.html_url +'">'+ pullRequest.html_url +'</a>)</li>';
+      });
+      html += '</ul>';
+    });
 
-    res.write('We have had ' + userCount + ' visits!\n');
-
-    res.end();
-
- 
-
+    response.write(html);
+    response.end();
+  });
 });
 
-server.listen(9090);
+function fetchRepos (callback) {
+  _options.path = '/orgs/'+ app.get('ORGANISATION') +'/repos';
 
-console.log('server running...')
+  // Fetch the list of repos for a given organisation
+  var request = https.get(_options, function (res) {
+    data = "";
 
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function () {
+      var repos = JSON.parse(data);
+      return callback(repos);
+    });
+  });
+
+  request.on('error', function (error) {
+    console.log('Problem with request: '+ error);
+  });
+}
+
+function fetchPullRequests (repos) {
+  var pullRequests = [];
+  _.each(repos, function (repo, index) {
+    _options.path = '/repos/'+ app.get('ORGANISATION') +'/'+ repo.name +'/pulls';
+    var request = https.get(_options, function (res) {
+      (function (repo) {
+        var data = "";
+
+        res.on('data', function (chunk) {
+          data += chunk;
+        });
+
+        res.on('end', function () {
+          data = JSON.parse(data);
+          if (data.length > 0) {
+            pullRequests.push({repo: repo.name, data: data});
+          }
+
+          if (index == (repos.length - 1)) {
+            app.emit('pull-requests:fetched', pullRequests);
+          }
+        });
+      })(repo);
+    });
+  });
+}
+
+var port = Number(process.env.PORT || 5000);
+var server = app.listen(port, function () {
+  console.log('Listening on port %d', server.address().port);
+});
